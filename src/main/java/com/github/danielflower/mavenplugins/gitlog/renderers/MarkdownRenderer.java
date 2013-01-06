b2a704bb7bcb82f8db87de952127dfb24d67c158
Gerd Zanker <gerd.zanker@web.de>
2013-01-06 22:07:48 +0100

    Refactored code for easier testing. Added test cases for the markdown renderer and the markdown link converter. OPEN - task 10: Generate markdown formatted gitlog https://github.com/danielflower/maven-gitlog-plugin/issues/issue/10

--- src/main/java/com/github/danielflower/mavenplugins/gitlog/renderers/MarkdownRenderer.java
+++ src/main/java/com/github/danielflower/mavenplugins/gitlog/renderers/MarkdownRenderer.java
@@ -4,8 +4,6 @@ import static com.github.danielflower.mavenplugins.gitlog.renderers.Formatter.NE
 
 import java.io.File;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.maven.plugin.logging.Log;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -34,42 +32,6 @@ public class MarkdownRenderer extends FileRenderer {
 	private final boolean fullGitMessage;
 	protected final MessageConverter messageConverter;
 	private MarkdownLinkConverter markdownLinkConverter;
-	
-	/**
-	 * Helper class to convert a HTML link into a markdown link.
-	 * 
-	 * It searches for a <a href="...URL...">...Text...</a> and the
-	 * URL and text is used to build afterwards the markdown link.
-	 * The markdown links goes like [...Text...](...URL...).
-	 *
-	 */
-	private class MarkdownLinkConverter {
-
-		private Pattern pattern;
-		private final Log log;
-		
-		public MarkdownLinkConverter(Log log) {
-			this.log = log;
-			// see http://stackoverflow.com/questions/26323/regex-to-parse-hyperlinks-and-descriptions
-			// limited to double quotes (removed the single quote),
-			// because the Converters will use double quotes.
-			this.pattern = Pattern.compile("<a\\s+href=(\"([^\"]+)\").*?>(.*?)</a>");
-		}
-		
-		public String formatCommitMessage(String original) {
-			try {
-				Matcher matcher = pattern.matcher(original);
-				String result = matcher.replaceAll("[$3]($2)");
-				return result;
-			} catch (Exception e) {
-				// log, but don't let this small setback fail the build
-				log.info("Unable to convert a HTML link into markdown link: " + original, e);
-			}
-			return original;
-		}
-		
-	}
-	
 	
 	public MarkdownRenderer(Log log, File targetFolder, String filename, boolean fullGitMessage, MessageConverter messageConverter) throws IOException {
 		super(log, targetFolder, filename);
@@ -120,10 +82,21 @@ public class MarkdownRenderer extends FileRenderer {
 	public void renderFooter() throws IOException {
 	}
 
-	protected static String markdownEncode(String input) {
+	
+	/**
+	 * Helper function to escape some important characters for the
+	 * markdown hyperlink.
+	 * A backslash will be added in front of [ and ].
+	 * At least these characters need to be escaped in markdown,
+	 * else the markdown hyperlink will not work.
+	 * 
+	 * The function is package public for easy unit tests.
+	 * 
+	 * @param input		string to escape
+	 * @return			escaped string
+	 */
+	static String markdownEncode(String input) {
 		// Add a backslash in front of [ and ]
-		// because (at least) these characters need to be escaped in markdown,
-		// else the markdown hyperlink will not work.
 		input = input.replaceAll("\\[", "\\\\[");
 		input = input.replaceAll("\\]", "\\\\]");
 		return input; 
