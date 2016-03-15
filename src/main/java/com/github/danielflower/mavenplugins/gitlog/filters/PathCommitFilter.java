f6810ac47bdafd1b3ab25ebefb85b574ae86cd31
helpermethod <weiler@predic8.de>
2016-03-15 10:25:22 +0100

    Add support for specifiying absolute paths

--- src/main/java/com/github/danielflower/mavenplugins/gitlog/filters/PathCommitFilter.java
+++ src/main/java/com/github/danielflower/mavenplugins/gitlog/filters/PathCommitFilter.java
@@ -8,38 +8,49 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.util.io.DisabledOutputStream;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 /**
  * Filters out commits not related to files under a given path.
  *
- * @author Oliver Weiler (weiler@predic8.de)
+ * @author helpermethod
  */
 public class PathCommitFilter implements CommitFilter {
     private final Repository repository;
-    private final String path;
+    private final String relativizedPath;
     private final Log log;
 
     public PathCommitFilter(Repository repository, String path, Log log) {
         this.repository = repository;
-        this.path = path;
+        this.relativizedPath = relativizePath(repository, path);
         this.log = log;
+    }
+
+    private String relativizePath(Repository repository, String path) {
+        File p = new File(path);
+
+        return p.isAbsolute() ? repository.getDirectory().getParentFile().toURI().relativize(p.toURI()).getPath() : path;
     }
 
     @Override
     public boolean renderCommit(RevCommit commit) {
+        if (commit.getParentCount() != 1) {
+            return false;
+        }
+
         try {
             return isFoundInPath(commit);
         } catch (IOException e) {
             log.warn("Error while diffing commits.  Filter won't be applied.", e);
-            return true;
+            return false;
         }
     }
 
     private boolean isFoundInPath(RevCommit commit) throws IOException {
         for (DiffEntry diff : getDiffs(commit, commit.getParent(0))) {
-            if (diff.getNewPath().startsWith(path)) {
+            if (diff.getNewPath().startsWith(relativizedPath)) {
                 return true;
             }
         }
