2d71278585fd775b3a619548388f42190d2fbc4f
Daniel Flower <git@danielflower.com>
2023-02-18 19:59:03 +0800

    Putting debug logging

--- src/test/java/com/github/danielflower/mavenplugins/gitlog/ActionsTest.java
+++ src/test/java/com/github/danielflower/mavenplugins/gitlog/ActionsTest.java
@@ -4,25 +4,22 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.stream.Stream;
 
 public class ActionsTest {
 
 	@Test
 	public void canDoIt() throws Exception {
 
-		String githubActor = System.getenv("GITHUB_ACTOR");
-		String githubToken =  System.getenv("GITHUB_TOKEN");
+		String githubActor = ""; //System.getenv("GITHUB_ACTOR");
+		String githubToken = System.getenv("GITHUB_TOKEN");
 		System.out.println("githubActor = " + hide(githubActor));
 		System.out.println("githubToken = " + hide(githubToken));
 
-		Thread.sleep(Long.MAX_VALUE);
-		Git git = Git.open(new File(""));
+		Git git = Git.open(new File("D:\\code\\gittest"));
 		CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(githubActor, githubToken);
 		Collection<Ref> response = git.lsRemote()
 				.setTags(true).setHeads(false)
