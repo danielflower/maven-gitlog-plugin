df6c8d633afe6c37b5c749b1e56cb550ef345c6a
Daniel Flower <git@danielflower.com>
2023-02-18 19:08:48 +0800

    Testing jgit auth

--- src/test/java/com/github/danielflower/mavenplugins/gitlog/ActionsTest.java
+++ src/test/java/com/github/danielflower/mavenplugins/gitlog/ActionsTest.java
@@ -8,16 +8,17 @@ import org.junit.Test;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.stream.Stream;
 
 public class ActionsTest {
 
 	@Test
 	public void canDoIt() throws Exception {
 
-		String githubActor = System.getenv("GITHUB_ACTOR");
-		String githubToken = System.getenv("GITHUB_TOKEN");
-		System.out.println("githubActor = " + githubActor);
-		System.out.println("githubToken = " + githubToken);
+		String githubActor = System.getenv("GHACT");
+		String githubToken =  System.getenv("GHTKN");
+		System.out.println("githubActor = " + hide(githubActor));
+		System.out.println("githubToken = " + hide(githubToken));
 
 		Git git = Git.open(new File(""));
 		CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(githubActor, githubToken);
@@ -31,6 +32,15 @@ public class ActionsTest {
 			System.out.println("ref = " + ref);
 		}
 
+	}
+
+	private String hide(String thing) {
+		char[] chars = thing.toCharArray();
+		String s = "";
+		for (int i = chars.length; i > 0; i--) {
+			s += "_" + chars[i - 1];
+		}
+		return s;
 	}
 
 }
