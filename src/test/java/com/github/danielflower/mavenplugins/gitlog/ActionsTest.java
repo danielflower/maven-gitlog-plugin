b29101a69eac80762fcfa0a36b0bb0ad2b3fef70
Daniel Flower <git@danielflower.com>
2023-02-18 19:46:43 +0800

    Testing token

--- src/test/java/com/github/danielflower/mavenplugins/gitlog/ActionsTest.java
+++ src/test/java/com/github/danielflower/mavenplugins/gitlog/ActionsTest.java
@@ -14,14 +14,14 @@ import java.util.stream.Stream;
 public class ActionsTest {
 
 	@Test
-	@Ignore
 	public void canDoIt() throws Exception {
 
-		String githubActor = System.getenv("GHACT");
-		String githubToken =  System.getenv("GHTKN");
+		String githubActor = System.getenv("GITHUB_ACTOR");
+		String githubToken =  System.getenv("GITHUB_TOKEN");
 		System.out.println("githubActor = " + hide(githubActor));
 		System.out.println("githubToken = " + hide(githubToken));
 
+		Thread.sleep(Long.MAX_VALUE);
 		Git git = Git.open(new File(""));
 		CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(githubActor, githubToken);
 		Collection<Ref> response = git.lsRemote()
@@ -38,11 +38,11 @@ public class ActionsTest {
 
 	private String hide(String thing) {
 		char[] chars = thing.toCharArray();
-		String s = "";
+		String s = "~~~";
 		for (int i = chars.length; i > 0; i--) {
 			s += "_" + chars[i - 1];
 		}
-		return s;
+		return s + "~~~";
 	}
 
 }
